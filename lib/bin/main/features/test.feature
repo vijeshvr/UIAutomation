22e8680abb5089c6cdac3e5dfc68f83c00f263f7
Vijesh V R <vijeshvr182@gmail.com>
2023-07-09 23:29:19 +0530

    second commit

--- lib/bin/main/features/test.feature
+++ lib/bin/main/features/test.feature
@@ -4,63 +4,55 @@ Feature: CustomerRegistration
   I want to use this template for my feature file
 
   @positive
-  Scenario: Add a new Customer 
+  Scenario Outline: Add a new Customer 
     Given User is in Managers page
     Then User Clicks the New Customer button
     When User is in new Customer Entry Page
-    Then  User enters all the Customer details
+    Then User enters <Name>, <Gender>, <Address>, <City>, <State>, <PIN>, <MobileNumber>, <Email>, <Password>
     And User Clicks submit button
     Then User lands on Sucessful Customer Registration page
-    
-    @positive
+     
+    Examples: 
+      | Name  | Gender | Address  | City | State  | PIN  | MobileNumber | Email  | Password  |
+      | Gino |     Male | Paradise |  Chennai |   TamilNadu | 629163 	| 123456789 |     dbhph@hjs.com | Test9355 | 
+      #| Vije |     Male | Paradise |  Chennai |   TamilNadu | 629163 	| 123498889 |     dssh@hjs.com | Test5362 | 
+  @positive
   Scenario: Edit the Customer Phone number
     Given User is in Managers page
     Then User clicks on edit Customer buttton
     When User is in Edit Customer Page
     And User enters Customer id and click submit
-    When User is in Edit Customer Page
     Then User edit the Phone number and Click submit button
     Then User get a Changes made sucessfully message
-    #
-    #@negative
-  #Scenario: Create a savings account without initial deposit
-    #Given User is in Managers page
-    #Then User clicks on new account buttton
-    #When User is in add new account page
-    #Then User enters customer id, account type and click submit button
-    #Then User get Please fill all fields message
-    #
-    #@positive
-  #Scenario: Create  account for a customer
-    #Given User is in Managers page
-    #Then User clicks on new account buttton
-    #When User is in add new account page
-    #Then User enters customer id, account type and initial amount and click submit button
-    #Then User get Account Generated Successfully!!! text
-    #
-    #
-    #@negative
-  #Scenario: Delete the customer without removing the Customer account
-    #Given User is in Managers page
-    #Then User clicks on Delete Customer buttton
-    #When User is in delete Customer page
-    #Then User Enters the customer id and clicks submit
-    #Then User an alert meassage on customer deletion confirmation
-    #Then User Acepts the alert meassge
-    #Then User gets a alert message saying Customer could not be deleted!!. First delete all accounts of this customer then delete the customer
-    #
-    #
-    #@positive
-  #Scenario: Delete the customer  account
-    #Given User is in Managers page
-    #Then User clicks on Delete account buttton
-    #When User is in delete account page
-    #Then User Enters the account number and clicks submit
-    #Then User an alert meassage on account deletion confirmation
-    #Then User Acepts the alert meassge
-    #Then User gets a Account deleted Sucessfully message
-    #
-    #
+    
+    @negative
+  Scenario: Create a savings account without initial deposit
+    Given User is in Managers page
+    Then User clicks on new account buttton
+    When User is in add new account page
+    Then User enters customer id, account type and click submit button
+    Then User get Please fill all fields message
+    
+    @positive
+  Scenario: Create  account for a customer
+    Given User is in Managers page
+    Then User clicks on new account buttton
+    When User is in add new account page
+    Then User enters customer id, account type and initial amount and click submit button
+    Then User get Account Generated Successfully!!! text
+    
+    
+    @negative
+  Scenario: Delete the customer without removing the Customer account
+    Given User is in Managers page
+    Then User clicks on Delete Customer buttton
+    When User is in delete Customer page
+    Then User Enters the customer id and clicks submit
+    Then User gets an alert meassage on customer deletion confirmation
+    Then User gets a alert message saying Customer could not be deleted!!. First delete all accounts of this customer then delete the customer
+    
+    
+    
 
 
   #@tag2
--- lib/src/main/java/stepDefinition/MyStepDefinition.java
+++ lib/src/main/java/stepDefinition/MyStepDefinition.java
@@ -3,6 +3,7 @@ package stepDefinition;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +15,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -21,33 +23,23 @@ import junit.framework.Assert;
 
 public class MyStepDefinition {
 	
-	public WebDriver driver;
+	public static WebDriver driver;
 	
-	public void invokeBrowser() {
+	public static String CustomerID;
+	public static String AccountNumber;
 	
-//	System.setProperty("webdriver.edge.driver", "C:\\Users\\Vijesh V R\\eclipse-workspace\\DemoBDDCucumber\\lib\\src\\main\\resources\\driver\\msedgedriver.exe");
-//	WebDriver driver = new EdgeDriver();
-		
-//		System.setProperty("webdriver.ie.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\driver\\IEDriverServer.exe");
-//		 driver= new InternetExplorerDriver();
-//		 driver.manage().window().maximize();
-//
-//		 
-//	
-//	driver.get("https://demo.guru99.com/V4/manager/Managerhomepage.php");
-	}
+	
 
 	
 	@Given("User is in Managers page")
-	public void user_is_in_managers_page() {
-	    invokeBrowser();
+	public static void user_is_in_managers_page() {
+	   
 	    System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\driver\\geckodriver.exe");
-//		 driver= new InternetExplorerDriver();
+
 		 
-	 // Set the path to the ad blocker extension XPI file
+//	  Set the path to the ad blocker extension XPI file
         String adBlockerExtensionPath = "C:\\Users\\Vijesh V R\\eclipse-workspace\\DemoBDDCucumber\\lib\\src\\main\\resources\\driver\\uBlock0_1.50.1b6.firefox.signed.xpi";
 
-//        File adBlockerExtension = new File(adBlockerExtensionPath);
         FirefoxProfile profile = new FirefoxProfile();
         
         profile.setPreference("extensions.enabledAddons", "ublock-origin@raymondhill.net:" + adBlockerExtensionPath);
@@ -72,41 +64,24 @@ public class MyStepDefinition {
 		 WebElement uid =driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input"));
 		 uid.click();
 		 uid.clear();
-		 uid.sendKeys("mngr507818");
+		 uid.sendKeys("mngr513520");
 		 
 		 WebElement pass =driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input"));
 		 pass.click();
 		 pass.clear();
-		 pass.sendKeys("arYgUtU");
+		 pass.sendKeys("AdUnudA");
 		 
 		 WebElement login =driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input[1]"));
 		 login.click();
-		 
-			        
-//	        WebElement addv = driver.findElement(By.xpath("/html/body/div/div/div[1]/div[1]/div/svg/path[1]"));
-//
-//	        // Click on the element
-//	        addv.click();
-//	        
-//	        try {
-//				Thread.sleep(2000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+			       
 		 
 		 System.out.println("Step 1 done");
 		 
-		 
-//		 
-//		 System.out.println("Hii");
-//		 driver.quit();
 	}
 	@Then("User Clicks the New Customer button")
-	public void user_clicks_the_new_customer_button() {
+	public static void user_clicks_the_new_customer_button() {
 		 WebElement element1 = driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[2]/a"));
 
-	        // Click on the element
 	        element1.click();
 	        
 	        System.out.println("Step 2 done");
@@ -114,7 +89,7 @@ public class MyStepDefinition {
 
 	}
 	@When("User is in new Customer Entry Page")
-	public void user_is_in_new_customer_entry_page() {
+	public static void user_is_in_new_customer_entry_page() {
 	    String ExpTitle1="Guru99 Bank New Customer Entry Page";
 	    
 	    String ActTitle1=driver.getTitle();
@@ -126,148 +101,306 @@ public class MyStepDefinition {
 		System.out.println("Step 3 done");
 
 	}
-	@Then("User enters all the Customer details")
-	public void user_enters_all_the_customer_details() {
+    @ParameterType(".*")
+    public String dynamicParameter(String value) {
+        return value;
+    }
+
+    @Then("User enters {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}, {dynamicParameter}")
+
+	public void user_enter(String Name, String Gender, String Address, String City, String State, String PIN, String MobileNumber, String Email, String Password) {
 		WebElement CName = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input"));
-		CName.sendKeys("name");
+		CName.sendKeys(Name);
 		
 		WebElement element1 = driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[2]/a"));
 
-		String Cgender = "Male";
+		String Cgender = Gender;
 
         if (Cgender.equals("Male")) {
            
             WebElement Male = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]"));
 
-	        // Click on the element
 	        Male.click();
         }else {
         	WebElement Female = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[2]"));
 
-	        // Click on the element
 	        Female.click();
         };
         
         WebElement dateInput = driver.findElement(By.id("dob"));
         
        
-//        try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        dateInput.sendKeys("12/12/2018");
-        
         
         JavascriptExecutor js=(JavascriptExecutor)driver; 
         js.executeScript("arguments[0].type = arguments[1]", dateInput, "text"); 
         driver.findElement(By.id("dob")).sendKeys("12/12/1999");
         
-//        JavascriptExecutor js2 = (JavascriptExecutor) driver;
-//        js2.executeScript("arguments[0].value = arguments[1]", dateInput,"23-03-2000");
-
-        
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("arguments[0].onkeyup()", dateInput);
-
-        
-        
         
         WebElement address = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[7]/td[2]/textarea"));
 
-        // Click on the element
+       
         address.clear();
-        address.sendKeys("iykyk");
+        address.sendKeys(Address);
         
         WebElement city = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[8]/td[2]/input"));
 
-        // Click on the element
+       
         city.clear();
-        city.sendKeys("Chennai");
+        city.sendKeys(City);
         
         WebElement state = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[9]/td[2]/input"));
 
-        // Click on the element
+      
         state.clear();
-        state.sendKeys("TN");
+        state.sendKeys(State);
         
         WebElement pin = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[10]/td[2]/input"));
 
-        // Click on the element
+       
         pin.clear();
-        pin.sendKeys("123456");
+        pin.sendKeys(PIN);
         
         WebElement phone = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[11]/td[2]/input"));
 
-        // Click on the element
+      
         phone.clear();
-        phone.sendKeys("1234567891");
+        phone.sendKeys(MobileNumber);
         
         WebElement email = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[12]/td[2]/input"));
 
-        // Click on the element
+       
         email.clear();
-        email.sendKeys("hjdh@hjs.com");
+        email.sendKeys(Email);
         
         WebElement password = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[13]/td[2]/input"));
 
-        // Click on the element
+
         password.clear();
-        password.sendKeys("Test12345");
+        password.sendKeys(Password);
         
         System.out.println("Customer details entered sucessfully");
-        
 	}
+	
 	@Then("User Clicks submit button")
-	public void user_clicks_submit_button() {
-	   WebElement submit = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[14]/td[2]/input[1]"));
-	   submit.click();
+	public static void user_clicks_submit_button() {
+	   WebElement submit1 = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[14]/td[2]/input[1]"));
+	   submit1.click();
 	   System.out.println("User details submitted sucessfully");
 	   
 	}
 	@Then("User lands on Sucessful Customer Registration page")
-	public void user_lands_on_sucessful_customer_registration_page() {
+	public static void user_lands_on_sucessful_customer_registration_page() {
 	    WebElement UsrAddedMsg = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[1]/td/p"));
 	    
 	    if(UsrAddedMsg.getText().equalsIgnoreCase("Customer Registered Successfully!!!")) {
 	    	
 	    	System.out.println("Customer Registered");
-	    	String CustomerID =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]")).getText();
+	    	CustomerID =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]")).getText();
 	    	System.out.println(CustomerID);
 	    }else {
 	    	System.out.println("User not Added");
 	    }
+	    
+	    driver.quit();
 	}
 	@Then("User clicks on edit Customer buttton")
-	public void user_clicks_on_edit_customer_buttton() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+	public static void user_clicks_on_edit_customer_buttton() {
+		WebElement EditCus =driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[3]/a"));
+		EditCus.click();
+		
+	    }
 
 	@When("User is in Edit Customer Page")
-	public void user_is_in_edit_customer_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public static void user_is_in_edit_customer_page() throws Exception {
+		String ExTitle = "Guru99 Bank Edit Customer Page";
+		String AcTitle=driver.getTitle();
+		if (ExTitle.equalsIgnoreCase(AcTitle)) {
+        System.out.println("User is in "+ AcTitle);
+		} else {
+    	throw new Exception("Result is negative. Stopping execution.");
+		}
 	}
 
 	@When("User enters Customer id and click submit")
 	public void user_enters_customer_id_and_click_submit() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement EditCusId =driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td/table/tbody/tr[6]/td[2]/input"));
+		EditCusId.sendKeys(CustomerID);
+		WebElement submit2 = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td/table/tbody/tr[11]/td[2]/input[1]"));
+		   submit2.click();
 	}
 
 	@Then("User edit the Phone number and Click submit button")
 	public void user_edit_the_phone_number_and_click_submit_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement Editphone = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[11]/td[2]/input"));
+
+      
+        Editphone.clear();
+        Editphone.sendKeys("9999999999");
+        
+        WebElement submit3 = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[13]/td[2]/input[1]"));
+		   submit3.click();
 	}
 
 	@Then("User get a Changes made sucessfully message")
 	public void user_get_a_changes_made_sucessfully_message() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		
+		
+		Alert alert1 = driver.switchTo().alert();
+		
+		 String alertText1 = alert1.getText();
+		 
+	        System.out.println("Alert Text: " + alertText1);
+	        
+	        alert1.accept();
+	        
+	        driver.switchTo().defaultContent();
+	        
+	        driver.quit();
+	        
+	        String ExpAlertMsg = "Changes made to Customer records";
+	        
+	        Assert.assertEquals(ExpAlertMsg, alertText1);
+	        
+	        
+	   
 	}
+	
+	@Then("User clicks on new account buttton")
+	public void user_clicks_on_new_account_buttton() {
+		WebElement NewAcc =driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[5]/a"));
+		NewAcc.click();
+	}
+	@When("User is in add new account page")
+	public void user_is_in_add_new_account_page() throws Exception {
+		String ExTitle2 = "Guru99 bank add new account";
+		String AcTitle2=driver.getTitle();
+		if (ExTitle2.equalsIgnoreCase(AcTitle2)) {
+        System.out.println("User is in "+ AcTitle2);
+		} else {
+    	throw new Exception("Result is negative. Stopping execution.");
+		}	
+	}
+	
+	
+	@Then("User enters customer id, account type and click submit button")
+	public void user_enters_customer_id_account_type_and_click_submit_button() {
+		WebElement EditCus =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"));
+		EditCus.clear();
+		EditCus.sendKeys(CustomerID);
+		
+		WebElement InitialAmount =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input"));
+		InitialAmount.clear();
+//		InitialAmount.sendKeys("5000");
+		
+		WebElement Submit3 =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]"));
+		Submit3.click();
+		
+	}
+	@Then("User get Please fill all fields message")
+	public void user_get_please_fill_all_fields_message() {
+		Alert alert2 = driver.switchTo().alert();
+		
+		 String alertText2 = alert2.getText();
+		 
+	        System.out.println("Alert Text: " + alertText2);
+	        
+	        alert2.accept();
+	        driver.switchTo().defaultContent();
+	        
+	        driver.quit();
+	        
+	        String ExpAlertMsg = "Please fill all fields";
+	        
+	        Assert.assertEquals(ExpAlertMsg, alertText2);
+	        
+	}
+	
+	@Then("User enters customer id, account type and initial amount and click submit button")
+	public void user_enters_customer_id_account_type_and_initial_amount_and_click_submit_button() {
+		WebElement EditCus =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"));
+		EditCus.clear();
+		EditCus.sendKeys(CustomerID);
+		
+		WebElement InitialAmount =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input"));
+		InitialAmount.clear();
+		InitialAmount.sendKeys("5000");
+		
+		WebElement Submit3 =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]"));
+		Submit3.click();
+	}
+	@Then("User get Account Generated Successfully!!! text")
+	public void user_get_account_generated_successfully_text() {
+		 WebElement AccCreatedMsg = driver.findElement(By.xpath("//*[@id=\"account\"]/tbody/tr[1]/td/p"));
+		    
+		    if(AccCreatedMsg.getText().equalsIgnoreCase("Account Generated Successfully!!!")) {
+		    	
+		    	System.out.println("Account Created");
+		    	AccountNumber =driver.findElement(By.xpath("//*[@id=\"account\"]/tbody/tr[4]/td[2]")).getText();
+		    	System.out.println(AccountNumber);
+		    }else {
+		    	System.out.println("Account Not Created");
+		    }
+		    
+		    driver.quit();
+		    
+		    System.out.println(CustomerID);
+	}
+	
+	@Then("User clicks on Delete Customer buttton")
+	public void user_clicks_on_delete_customer_buttton() {
+		WebElement DelCus =driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[4]/a"));
+		DelCus.click();
+	}
+	@When("User is in delete Customer page")
+	public void user_is_in_delete_customer_page() throws Exception {
+		String ExTitle3 = "Guru99 Bank Delete Customer Page";
+		String AcTitle3=driver.getTitle();
+		if (ExTitle3.equalsIgnoreCase(AcTitle3)) {
+        System.out.println("User is in "+ AcTitle3);
+		} else {
+    	throw new Exception("Result is negative. Stopping execution.");
+		}
+	}
+	@Then("User Enters the customer id and clicks submit")
+	public void user_enters_the_customer_id_and_clicks_submit() {
+		WebElement DelCusId =driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"));
+		DelCusId.sendKeys(CustomerID);
+		WebElement submit3 = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/table/tbody/tr[7]/td[2]/input[1]"));
+		   submit3.click();
+	}
+	@Then("User gets an alert meassage on customer deletion confirmation")
+	public void user_an_alert_meassage_on_customer_deletion_confirmation() {
+		Alert alert3 = driver.switchTo().alert();
+		
+		 String alertText3 = alert3.getText();
+		 
+	        System.out.println("User get a Alert Text: " + alertText3);
+	        
+	        alert3.accept();
+	        driver.switchTo().defaultContent();
+	        
+	        try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+	}
+	
+	@Then("User gets a alert message saying Customer could not be deleted!!. First delete all accounts of this customer then delete the customer")
+	public void user_gets_a_alert_message_saying_customer_could_not_be_deleted_first_delete_all_accounts_of_this_customer_then_delete_the_customer() {
+		Alert alert4 = driver.switchTo().alert();
+		
+		 String alertText4 = alert4.getText();
+		 
+	        System.out.println("User get a Alert Text: " + alertText4);
+	        
+	        alert4.accept();
+	        driver.switchTo().defaultContent();
+	        driver.quit();
+	        
+	}
 	
 }
